ae494abbd2914ab417260febd17ef976e88e6db6
Harry <hte488@gmail.com>
2022-03-05 16:21:59 -0500

    Updated URL path

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:{}@localhost:5432/marvel'.format(os.environ.get('PGPASS'))
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'postgresql://wguemifjpyyqfg:24bc18386b5d3262dda45d2347ec577a070fb4f3fbf522dde490e6770ebc7a76@ec2-3-227-195-74.compute-1.amazonaws.com:5432/d73pd9ukavp8s7'
+SQLALCHEMY_DATABASE_URI = 'postgresql://iytxcroajwyzzj:dff683d8f87b26d2aeb6f25781555662cd5017366cb7cf1354e6dfc302f8e6fc@ec2-184-73-198-174.compute-1.amazonaws.com:5432/delskuejia3n6c'

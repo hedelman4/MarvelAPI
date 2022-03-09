81bfb2e9639ed2a23e5867f2e55a4e25b74490ab
Harry <hte488@gmail.com>
2022-03-09 18:54:20 -0500

    Second project revisions

--- auth.py
+++ auth.py
@@ -4,10 +4,11 @@ from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import config
 
-AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
-ALGORITHMS = os.environ.get('ALGORITHMS')
-API_AUDIENCE = os.environ.get('API_AUDIENCE')
+AUTH0_DOMAIN = config.AUTH0_DOMAIN
+ALGORITHMS = config.ALGORITHMS
+API_AUDIENCE = config.API_AUDIENCE
 
 class AuthError(Exception):
     def __init__(self, error, status_code):
--- config.py
+++ config.py
@@ -5,3 +5,7 @@ DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = 'postgresql://iytxcroajwyzzj:dff683d8f87b26d2aeb6f25781555662cd5017366cb7cf1354e6dfc302f8e6fc@ec2-184-73-198-174.compute-1.amazonaws.com:5432/delskuejia3n6c'
 database_path = 'postgresql://postgres:{}@localhost:5432/marvel'.format(os.environ.get('PGPASS'))
+AUTH0_DOMAIN='dev-r-0hid9s.us.auth0.com'
+ALGORITHMS=['RS256']
+API_AUDIENCE='Marvel'
+CLIENT_ID='8J2ZSyjaw2JDKf5MFIzomNuBbGRKdEr7'
